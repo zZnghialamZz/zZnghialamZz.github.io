14b5dde6ca315941d311ce826657db933fc53a97
Nghia Lam <nghialam12795@gmail.com>
2021-11-02 22:15:58 +0700

    WIP

---
+++ src/components/header.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import { logo } from "../assets/images";
+
+import "../scss/header.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__inner container">
+        <div className="header__logo">
+          <img src={logo} alt="" />
+        </div>
+        <ul className="header__nav">
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Experiences</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
