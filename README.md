a28e73cdc536f3a8a694250052c483292a36a841
Nghia Lam <nghialam12795@gmail.com>
2021-11-02 14:05:18 +0700

    Remove reduntdant content

--- README.md
+++ README.md
@@ -1,18 +1,4 @@
 # zZnghialamZz.github.io
+
 My personal website
 
-## Dependencies
-
-* Gatsby CLI
-* NPM
-
-## Install & Develop
-
-> npm install
->
-> npm run develop
-
-## Credit
-
-This website is originally designed and developed by 
-[Brittany Chiang](https://github.com/bchiang7/v4).
--- src/styles/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Theme } from './theme';
-export { default as GlobalStyle } from './globalStyle';
-export { default as Mixins } from './mixins';
-export { default as Media } from './media';
-export { default as Main } from './main';
-export { default as Section } from './section';
-export { default as Heading } from './heading';
-export { default as Header } from './header';
